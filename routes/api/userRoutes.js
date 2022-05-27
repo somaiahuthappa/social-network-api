79ca599908c8b11d3bdbb64f6ea42fffd7819fcb
Som's Laptop <somaiahuthappa@gmail.com>
2022-05-27 00:22:13 -0400

    added routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { get } = require('http');
 // import destructured methods
 const {
     getAllUsers,
@@ -9,6 +10,27 @@ const {
     addFriend,
     deleteFriend
 } = require('../../controllers/userController')
+
+// set up get all and post at /api/users
+
+router
+.route('/')
+.get(getAllUsers)
+.post(createUser)
+
+// set up get one user, update and delete at /api/users/:id
+
+router
+.route('/:id')
+get(getUserById)
+.put(updateUser)
+.delete(deleteUser)
+
+// add and delete friend
+router
+.route('/:id/friends/:friendsId')
+.post(addFriend)
+.delete(deleteFriend)
 
 
 module.exports = router;
